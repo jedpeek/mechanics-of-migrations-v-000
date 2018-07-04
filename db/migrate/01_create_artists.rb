f4088032ddf87c2eec4f597481438dea5a98b41f
Jed Peek <jedpeek@yahoo.com>
2018-07-04 17:37:32 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,21 +1,5 @@
 class CreateArtists < ActiveRecord::Migration
   def change
+    create_table :artists do |t|
   end
-
-  connection = ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
-
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
 end
